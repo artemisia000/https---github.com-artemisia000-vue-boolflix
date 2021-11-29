a035b484553ec2e909dc763ea165eeb521977175
artemisia000 <misiapierobon@gmail.com>
2021-11-29 14:13:38 +0100

    chiamata api

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+<header>
+
+    <form>
+        <input type="text"
+               class="control ms-3"
+               placeholder="Search your Movie"
+         
+        >
+    </form>
+
+    <button class="btn btn-primary "
+
+    >
+    Search
+
+    </button>
+
+     
+
+
+</header>
+  
+
+</template>
+
+<script>
+export default {
+    name: 'Header',
+
+}
+</script>
+
+<style  scoped lang="scss">
+
+header{
+    width: 100%;
+    height: 70px;
+
+    .btn{
+        height: 30px;
+        margin: 0 15px;
+    }
+}
+
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,48 @@
+<template>
+<main>
+
+
+</main>
+  
+
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'Main',
+
+
+data(){
+    return{
+        listMovie: [],
+    };
+},
+
+created(){
+    this.getMovie();
+
+},
+
+methods: {
+    getMovie(){
+
+    axios
+        .get('https://api.themoviedb.org/3/search/movie?api_key=01761d48304ba79f4b1135eba2934ae8&query=wes anderson')
+        .then(result => {
+            console.log(result.data);
+            this.listMovie = result.data;
+        })
+        .catch(error => console.log(error));
+    }
+
+  }
+
+};
+
+
+</script>
+
+<style>
+
+</style>
